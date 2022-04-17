18e13b9370ceb455a6f3c25b75511833d4dcd10d
Kia <dr.kia99@gmail.com>
2022-04-17 13:52:29 +0430

    add price add close button fix ul | li descriptions

--- js/app.js
+++ js/app.js
@@ -3,13 +3,17 @@ let addedItems = 0;
 let searchResultsCount = 0;
 let isSearching = false;
 let loadingTimeout, refreshTimeout;
+let data;
+let offset = 60;
 
 // * Header Elements
+const header = select("#header");
 const El_searchInput = select("#searchInput");
 const Di_searchInput = new TInput(El_searchInput, true);
 const El_toggleTheme = select("#toggleTheme");
 const Di_toggleTheme = new BInput(El_toggleTheme, true);
 // * Body Elements
+const overlay = select("#overlay");
 const mainContainer = select("#mainContainer");
 const resultsInfo = select("#resultsInfo");
 const nothing = select("#nothing");
@@ -82,7 +86,7 @@ Di_toggleTheme.onClick(() => {
   }
 });
 
-// Fetch
+//// Fetch
 
 const columns = [];
 const numberOfCols = Math.floor((window.innerWidth * 0.8) / 300);
@@ -95,41 +99,110 @@ for (let i = 0; i < numberOfCols; i++) {
 
 function addItem(ref, output, count) {
   // * Creating div.item
+  let img = new Image();
+  img.src = ref[count]["image_url"];
+
   const div_item = document.createElement("div");
   div_item.id = ref[count]["page_id"];
   div_item.setAttribute("class", "item");
-
+  // * Info Container
   const div_info = document.createElement("div");
   div_info.setAttribute("class", "info");
-
+  // * Title
   const h3_title = document.createElement("h3");
   h3_title.setAttribute("class", "title");
   h3_title.innerHTML = ref[count]["name"];
-
+  // * Description
   const p_des = document.createElement("p");
   p_des.setAttribute("class", "description");
-  const span_des = document.createElement("span");
-  span_des.innerHTML = ref[count]["description"];
-  p_des.appendChild(span_des);
-
+  p_des.innerHTML = ref[count]["description"];
+  // * Click to Expand text
   const div_more = document.createElement("div");
   div_more.setAttribute("class", "readMore");
   div_more.innerHTML = "Click to expand";
-
+  // * Close Button
+  const div_close = document.createElement("div");
+  div_close.setAttribute("class", "closeBtn");
+  div_close.innerHTML = "close";
+  // * Price
+  const p_price = document.createElement("p");
+  p_price.setAttribute("class", "price");
+  p_price.innerHTML = ref[count]["price"];
+  // * Domain
   const a_domain = document.createElement("a");
   a_domain.setAttribute("class", "domain");
   a_domain.href = ref[count]["canonical_url"];
   a_domain.innerHTML = ref[count]["domain"];
 
-  let img = new Image();
-  img.src = ref[count]["image_url"];
+  // ** On Click
+  div_item.addEventListener("click", () => {
+    if (div_item.classList.contains("expanded")) return;
+    div_info.style.opacity = 0;
+
+    img.style.opacity = 0;
+    img.style.left = 0;
+
+    setTimeout(() => {
+      div_item.classList.add("expanded");
+      div_info.style.transition = "all 0s";
+      img.style.transition = "all 0s";
+
+      img.style.width = "auto";
+      img.style.position = "fixed";
+      img.style.height = window.innerHeight - header.offsetHeight + "px";
+
+      div_info.style.position = "fixed";
+      div_info.style.right = 0;
+      div_info.style.width = mainContainer.offsetWidth - img.width + "px";
+      div_info.style.height = window.innerHeight - header.offsetHeight + "px";
+      setTimeout(() => {
+        div_info.style.transition = "all 0.3s ease-in-out";
+        img.style.transition = "all 0.3s ease-in-out";
+
+        img.style.opacity = 1;
+        img.style.left = mainContainer.offsetLeft + "px";
+
+        div_info.style.right = mainContainer.offsetLeft + "px";
+        div_info.style.opacity = 1;
+      }, 50);
+    }, 300);
+  });
+  div_close.addEventListener("click", () => {
+    img.style.opacity = 0;
+    img.style.left = 0;
+
+    div_info.style.opacity = 0;
+    div_info.style.right = 0;
+
+    setTimeout(() => {
+      div_item.classList.remove("expanded");
+      div_info.style.transition = "all 0s";
+      img.style.transition = "all 0s";
+
+      img.style.width = "100%";
+      img.style.position = "static";
+      img.style.height = "unset";
+
+      div_info.style.position = "absolute";
+      div_info.style.width = "100%";
+      div_info.style.height = "100%";
+      setTimeout(() => {
+        img.style.transition = "all 0.3s ease-in-out";
+        div_info.style.transition = "all 0.3s ease-in-out";
+        img.style.opacity = 1;
+        div_info.style.opacity = "";
+      }, 50);
+    }, 300);
+  });
 
   div_info.appendChild(h3_title);
+  div_info.appendChild(div_close);
   div_info.appendChild(p_des);
+  div_info.appendChild(p_price);
   div_info.appendChild(div_more);
   div_info.appendChild(a_domain);
-  div_item.appendChild(img);
   div_item.appendChild(div_info);
+  div_item.appendChild(img);
 
   // *
   let minHeight = null;
@@ -173,10 +246,17 @@ function addItem(ref, output, count) {
 
     setTimeout(() => {
       showItem(div_item);
+      // So the height doesn't depend on the image anymore
+      div_item.style.minHeight = div_item.offsetHeight + "px";
     }, 300);
   };
 }
-addItem(data, columns, addedItems);
+fetch("http://xoosha.com/ws/1/test.php?offset=0")
+  .then((res) => res.json())
+  .then((newData) => {
+    data = newData;
+    addItem(data, columns, addedItems);
+  });
 
 document.addEventListener("scroll", (ev) => {
   let bodyHeight = document.body.offsetHeight;
@@ -185,10 +265,15 @@ document.addEventListener("scroll", (ev) => {
     !isLoading &&
     bodyHeight - window.scrollY < window.innerHeight + 200
   ) {
-    //// console.log("now");
-    data = data.concat(data2);
     isLoading = true;
     showFooterLoading(true);
-    addItem(data, columns, addedItems);
+    fetch(`http://xoosha.com/ws/1/test.php?offset=${offset}`)
+      .then((res) => res.json())
+      .then((newData) => {
+        console.log(`http://xoosha.com/ws/1/test.php?offset=${offset}`);
+        data = data.concat(newData);
+        addItem(data, columns, addedItems);
+        offset += 60;
+      });
   }
 });
